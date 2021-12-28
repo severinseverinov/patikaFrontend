10e0f36a56aa15508a9b4a365f06447ff70fec70
Severin ISAEV <severinisaev@hotmail.com>
2021-12-28 10:49:10 +0300

    added asianKitchen | javascript

--- javascript/deneme2/js/script.js
+++ javascript/deneme2/js/script.js
@@ -1,17 +1,3 @@
-const USER=["Ayse","Ahmet","MEHMET","ALI","CevDET"]
-
-const NEW_USER=USER.map(user => user.toLowerCase())
-
-console.log(NEW_USER)
-
-const USER_OBJ=USER.map( item =>
-    {
-        return{
-            username:item,
-            shortName:`${item[0]}.`,
-            newName:`${item[0].toUpperCase()}${item.slice(1).toLowerCase()}`
-    }
-}
-)
-
-console.log(USER_OBJ)
+let person={name:"Selin",city:"Ankara",favoriteColor:"aqua blue"}; 
+let {name:foo}=person;
+console.log(foo)
